50be7f54fe1c9d77b4e721cfbcee4027cbb76564
Brandon Norris <blnorris@gmail.com>
2020-11-07 13:48:35 -0600

    asd

--- test.lua
+++ test.lua
@@ -8,11 +8,11 @@ os.loadAPI("u")
 local  PATH = "quarryConfig"
 local x, y, z = gps.locate(5)
 
-
+local dir = u.GetDirection()
 
 print("testing")
-print(x,y,z)
-u.SaveStartPoint(x,y,z)
+print(x,y,z, dir)
+u.SaveStartPoint(x,y,z, dir)
 print("reading files" )
-local a,b,c = u.GetStartPoint()
-print(a,b,c)
+local a,b,c,d = u.GetStartPoint()
+print(a,b,c,d)
