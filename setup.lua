42f669aadddcf979554109ef3757ce0ce0239e11
Brandon Norris <blnorris@gmail.com>
2020-12-02 15:13:17 -0600

    sdfs

--- setup.lua
+++ setup.lua
@@ -62,7 +62,7 @@ shell.run("rm circle")
 shell.run("wget https://raw.githubusercontent.com/BLNorris/quarry/master/circle.lua circle")
 
 
-shell.run("rm startup")
+-- shell.run("rm startup")
 -- shell.run("copy q startup")
 -- io.write("Setup Quarry? (y/n):")
 -- quarry = io.read()
