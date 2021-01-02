044fff492cd3f5d1a5bd615c515628e99b6ac674
Brandon Norris <blnorris@gmail.com>
2021-01-02 06:23:44 -0600

    asdf

--- wkill.lua
+++ wkill.lua
@@ -1,9 +1,10 @@
 os.loadAPI("u")
 u.OpenModem()
 while true do
-    senderId, message, protocol = rednet.receive("placewither",1)
+    senderId, message, protocol = rednet.receive("placewither")
     if message == "place" then
+        turtle.up()
         turtle.place()
+        turtle.down()
     end
-    turtle.attack()
 end
